8032288244385c6c11f264d3ab35e3f553e580b4
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2019-03-14 18:18:37 +0100

    test

---
+++ src/main/java/com/kodilla/libraryapi/KodillaLibraryApiApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.libraryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaLibraryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaLibraryApiApplication.class, args);
+    }
+
+}
