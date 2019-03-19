6f0b1ad78dd9927bc05405cd57b887a35acb8b80
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2019-03-19 16:45:17 +0100

    start testing

---
+++ src/test/java/com/kodilla/libraryapi/service/UserServiceTest.java
@@ -0,0 +1,43 @@
+package com.kodilla.libraryapi.service;
+
+import com.kodilla.libraryapi.domain.User;
+import com.kodilla.libraryapi.enumerics.Currency;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserServiceTest {
+    @Autowired
+    private UserService userService;
+
+    @Transactional
+    @Test
+    public void testGettingUserById() {
+        //Given
+        User testUser = new User();
+        testUser.setName("John");
+        testUser.setSurname("Rambo");
+        testUser.setHasAdminRights(false);
+        testUser.setPrefferedCurrency(Currency.PLN);
+        testUser.setRegistrationDate(LocalDate.now());
+
+        //When
+        userService.addUser(testUser);
+        long userId = testUser.getId();
+        User loadedUser = userService.getUserById(userId);
+
+        //Then
+        Assert.assertTrue(loadedUser.getName().equals("John"));
+
+
+    }
+
+}
